2cc45fab2457b44efdc74e6c064a2184863fe952
Belloshifawu <shifawubello6@gmail.com>
2023-09-19 05:41:52 -0700

    updated

--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _isdigit(int num);
@@ -9,4 +12,5 @@ int _strlen(char *str);
 int _numlen(int *num);
 void printnumber(int number);
 int countdigits(int number);
+
 #endif
